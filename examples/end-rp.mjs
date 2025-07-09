b33a462540e5f1c2248708df33807d995f85cbec
Mirko Mollik <mirko.mollik@eudi.sprind.org>
2025-07-09 21:15:34 +0200

    chore: fix build pipeline
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>

--- examples/end-rp.mjs
+++ examples/end-rp.mjs
@@ -12,25 +12,25 @@ const port = process.env.PORT || 3001;
  */
 app.use(express.json());
 app.post('/process', (req, res) => {
-  const presented = req.body;
-  console.log('Received webhook:');
-  console.log(JSON.stringify(presented, null, 2));
-  res.status(200).send({
-    citizen: {
-      town: `You live in ${presented.credentials[0].address.locality}`,
-    },
-  });
+    const presented = req.body;
+    console.log('Received webhook:');
+    console.log(JSON.stringify(presented, null, 2));
+    res.status(200).send({
+        citizen: {
+            town: `You live in ${presented.credentials[0].address.locality}`,
+        },
+    });
 });
 
 /**
  * Receive the data the got received from the middleware
  */
 app.post('/consume', (req, res) => {
-  const presented = req.body;
-  console.log('Received consume webhook:', presented);
-  res.status(200).send();
+    const presented = req.body;
+    console.log('Received consume webhook:', presented);
+    res.status(200).send();
 });
 
 app.listen(port, () => {
-  console.log(`EUDIPLO handler running on http://localhost:${port}`);
+    console.log(`EUDIPLO handler running on http://localhost:${port}`);
 });
