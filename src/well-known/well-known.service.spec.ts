b5dc924d6b7fdde2de89f4d4847387e48f8ec8b9
Mirko Mollik <mirko.mollik@eudi.sprind.org>
2025-07-30 13:22:26 +0200

    chore: fix linting
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>

--- src/well-known/well-known.service.spec.ts
+++ src/well-known/well-known.service.spec.ts
@@ -1,18 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WellKnownService } from './well-known.service';
+import { describe, beforeEach, it, expect } from 'vitest';
 
 describe('WellKnownService', () => {
-  let service: WellKnownService;
+    let service: WellKnownService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [WellKnownService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [WellKnownService],
+        }).compile();
 
-    service = module.get<WellKnownService>(WellKnownService);
-  });
+        service = module.get<WellKnownService>(WellKnownService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
