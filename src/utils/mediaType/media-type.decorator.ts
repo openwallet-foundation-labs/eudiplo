9293754cb8b376a8a6bd1b9458230d2327b20e76
Lukas.J Han <lukas@hopae.io>
2025-07-30 12:07:39 +0900

    lint: fix
    Signed-off-by: Lukas.J Han <lukas@hopae.io>

--- src/utils/mediaType/media-type.decorator.ts
+++ src/utils/mediaType/media-type.decorator.ts
@@ -3,6 +3,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const ContentType = createParamDecorator(
     (data: unknown, ctx: ExecutionContext) => {
         const request = ctx.switchToHttp().getRequest();
-        return request.headers['content-type'];
+        return request.headers['content-type'] as string | undefined;
     },
 );
