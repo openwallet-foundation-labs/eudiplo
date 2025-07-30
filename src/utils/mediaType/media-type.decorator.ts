10c8730463a6f43794c188edc90049669815d6cf
Lukas.J Han <lukas@hopae.io>
2025-07-30 12:02:56 +0900

    feat: add signed metadata feature for issuer
    Signed-off-by: Lukas.J Han <lukas@hopae.io>

---
+++ src/utils/mediaType/media-type.decorator.ts
@@ -0,0 +1,8 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const ContentType = createParamDecorator(
+    (data: unknown, ctx: ExecutionContext) => {
+        const request = ctx.switchToHttp().getRequest();
+        return request.headers['content-type'];
+    },
+);
--- src/well-known/well-known.controller.ts
+++ src/well-known/well-known.controller.ts
@@ -6,6 +6,8 @@ import { JwksResponseDto } from './dto/jwks-response.dto';
 import { Oauth2AuthorizationServerResponse } from './dto/oauth-authorization-server-response.dto';
 import { CredentialIssuerMetadataDto } from './dto/credential-issuer-metadata.dto';
 import { ApiOperation } from '@nestjs/swagger';
+import { ContentType } from '../utils/mediaType/media-type.decorator';
+import { MediaType } from '../utils/mediaType/media-type.enum';
 
 /**
  * Controller for the OpenID4VCI well-known endpoints.
@@ -23,9 +25,34 @@ export class WellKnownController {
         description: 'Returns the OpenID4VCI issuer metadata.',
     })
     @Get('openid-credential-issuer')
-    async issuerMetadata(@Param('tenantId') tenantId: string) {
-        return (await this.oid4vciService.issuerMetadata(tenantId))
-            .credentialIssuer as unknown as Promise<CredentialIssuerMetadataDto>;
+    async issuerMetadata(
+        @Param('tenantId') tenantId: string,
+        @ContentType() contentType: MediaType,
+    ) {
+        /**
+           - an unsigned JSON document using the media type application/json, or
+           - a signed JSON Web Token (JWT) containing the Credential Issuer Metadata in its payload using the media type application/jwt.
+         */
+        const metadata = (await this.oid4vciService.issuerMetadata(tenantId))
+            .credentialIssuer as unknown as CredentialIssuerMetadataDto;
+        if (contentType === MediaType.APPLICATION_JWT) {
+            return this.cryptoService.signJwt(
+                {
+                    // [Review]: Could I get `alg` value here? or kid??
+                    typ: 'openidvci-issuer-metadata+jwt',
+                },
+                {
+                    ...metadata,
+                    // [Review]: could `iss` value same with `sub` or not?
+                    iss: metadata.credential_issuer,
+                    sub: metadata.credential_issuer,
+                    iat: Math.floor(new Date().getTime() / 1000),
+                    // [Review]: should we add `exp` value here?
+                },
+                tenantId,
+            );
+        }
+        return metadata;
     }
 
     /**
