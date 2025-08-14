4d329fe971c119e8a2cd714f356d5fc23a3cd991
Mirko Mollik <mirko.mollik@eudi.sprind.org>
2025-08-14 18:12:57 +0200

    fix: add correct url
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>

--- apps/client/public/env.js
+++ apps/client/public/env.js
@@ -2,6 +2,6 @@
   window['env'] = window['env'] || {};
 
   // Environment variables
-  window['env']['apiUrl'] = 'https://client.eudiplo.eudi-wallet.dev';
+  window['env']['apiUrl'] = 'https://eudiplo.eudi-wallet.dev';
   window['env']['oidcUrl'] = 'https://auth.eudi-wallet.dev/realms/eudiplo';
 })(this);
