edcc3751136a257f059db020388ce12eff41c418
Mirko Mollik <mirko.mollik@eudi.sprind.org>
2025-09-05 13:39:54 +0200

    fix: registrar init (#105)
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>

--- apps/backend/src/registrar/registrar.service.ts
+++ apps/backend/src/registrar/registrar.service.ts
@@ -1,9 +1,5 @@
 import { OAuth2Client } from "@badgateway/oauth2-client";
-import {
-    Injectable,
-    OnApplicationBootstrap,
-    OnModuleInit,
-} from "@nestjs/common";
+import { Injectable, OnModuleInit } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
@@ -26,7 +22,7 @@ import { client } from "./generated/client.gen";
  * including adding relying parties, access certificates, and registration certificates.
  */
 @Injectable()
-export class RegistrarService implements OnApplicationBootstrap, OnModuleInit {
+export class RegistrarService implements OnModuleInit {
     /**
      * OAuth2 client for interacting with the OIDC provider.
      */
@@ -57,7 +53,7 @@ export class RegistrarService implements OnApplicationBootstrap, OnModuleInit {
     /**
      * Initializes the OAuth2 client and registrar client with the necessary configurations.
      */
-    onModuleInit() {
+    async onModuleInit() {
         //when not set, we will not use the registrar
         if (!this.isEnabled()) {
             return;
@@ -84,6 +80,11 @@ export class RegistrarService implements OnApplicationBootstrap, OnModuleInit {
             baseUrl: this.configService.getOrThrow<string>("REGISTRAR_URL"),
             auth: () => this.accessToken,
         });
+
+        if (!this.configService.get<string>("REGISTRAR_URL")) {
+            return;
+        }
+        await this.refreshAccessToken();
     }
 
     /**
@@ -92,17 +93,6 @@ export class RegistrarService implements OnApplicationBootstrap, OnModuleInit {
      */
     isEnabled() {
         return !!this.configService.get<string>("REGISTRAR_URL");
-    }
-
-    /**
-     * This function is called when the application starts.
-     * It will refresh the access token for the registrar.
-     */
-    async onApplicationBootstrap() {
-        if (!this.configService.get<string>("REGISTRAR_URL")) {
-            return;
-        }
-        await this.refreshAccessToken();
     }
 
     /**
@@ -204,6 +194,7 @@ export class RegistrarService implements OnApplicationBootstrap, OnModuleInit {
         const host = this.configService
             .getOrThrow<string>("PUBLIC_URL")
             .replace("https://", "");
+        console.log(tenantId);
         return accessCertificateControllerRegister({
             client: this.client,
             body: {
