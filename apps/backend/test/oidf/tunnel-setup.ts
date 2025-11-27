88a96cb0c87df460568b4f2316490a4e54a9ad78
Mirko Mollik <mirko.mollik@eudi.sprind.org>
2025-11-27 17:10:48 +0100

    fix: print more
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>

--- apps/backend/test/oidf/tunnel-setup.ts
+++ apps/backend/test/oidf/tunnel-setup.ts
@@ -15,9 +15,38 @@ export function setupTunnel(): Promise<void> {
 
             new ConfigHandler(tunnel);
 
+            handler.on("config", ({ config }) => {
+                console.log("Config", config);
+            });
+
             tunnel.on("url", (url) => {
                 console.log("Tunnel is ready at", url);
                 resolvePromise();
+            });
+
+            tunnel.on("connected", (connection) => {
+                console.log("Connected to", connection);
+            });
+
+            tunnel.on("disconnected", (connection) => {
+                console.log("Disconnected from", connection);
+            });
+
+            tunnel.on("stdout", (data) => {
+                console.log("Tunnel stdout", data);
+            });
+
+            tunnel.on("stderr", (data) => {
+                console.error("Tunnel stderr", data);
+            });
+
+            tunnel.on("exit", (code, signal) => {
+                console.log(
+                    "Tunnel exited with code",
+                    code,
+                    "and signal",
+                    signal,
+                );
             });
 
             tunnel.on("error", (error) => {
