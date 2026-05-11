d0d70059aa0110e6ccadea1fc4fcb57fd0c05d20
Mirko Mollik <mirko.mollik@eudi.sprind.org>
2026-05-11 22:47:44 +0200

    docs: add audit-log config docs and stabilize API links

--- apps/client/src/app/tenants/tenant-show/tenant-show.component.ts
+++ apps/client/src/app/tenants/tenant-show/tenant-show.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
-import {
-  tenantControllerGetTenant,
-  TenantEntity,
-} from '@eudiplo/sdk-core';
+import { tenantControllerGetTenant, TenantEntity } from '@eudiplo/sdk-core';
 
 import { ClientListComponent } from '../client/client-list/client-list.component';
 import { MatIconModule } from '@angular/material/icon';
@@ -47,7 +44,6 @@ export class TenantShowComponent implements OnInit {
       return;
     }
   }
-
 
   getCleanupModeLabel(mode?: string): string {
     switch (mode) {
